7653400f85b4bf573312c28a6f42ab36039a6a07
shery1010 <142799181+shery1010@users.noreply.github.com>
2025-08-06 10:13:44 +0530

    Add files via upload

---
+++ operations/subtract two numbers .py
@@ -0,0 +1,11 @@
+# Subtraction of two numbers
+
+# Input from user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Subtract the numbers
+result = num1 - num2
+
+# Display the result
+print("The subtraction of", num1, "and", num2, "is:", result)
