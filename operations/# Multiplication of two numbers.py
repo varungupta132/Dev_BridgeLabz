63b69a3e1b598c2dfb311fb6b65b19b711891e8b
shery1010 <142799181+shery1010@users.noreply.github.com>
2025-08-06 10:11:14 +0530

    Add files via upload

---
+++ operations/# Multiplication of two numbers.py
@@ -0,0 +1,11 @@
+# Multiplication of two numbers
+
+# Input from user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Multiply the numbers
+result = num1 * num2
+
+# Display the result
+print("The multiplication of", num1, "and", num2, "is:", result)
